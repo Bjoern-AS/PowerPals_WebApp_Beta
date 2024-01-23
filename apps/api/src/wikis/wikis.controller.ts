61a6ccf2a4df77067eb73c20948320e1fd8237bf
Bjoern-AS <Bjoern_schick@outlook.de>
2024-01-23 20:11:54 +0100

    Create wikis.controller.ts

---
+++ apps/api/src/wikis/wikis.controller.ts
@@ -0,0 +1,105 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipeBuilder,
+  Query,
+  Request,
+} from '@nestjs/common';
+import { WikisService } from './wikis.service';
+import { CreateWikiDto } from './dto/create-wiki.dto';
+import { UpdateWikiDto } from './dto/update-wiki.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { editFileName } from 'src/utils/utils';
+import { Role } from 'src/users/roles.decorator';
+import { UserType } from 'src/users/entities/user.entity';
+import { Public } from 'src/auth/auth.decorator';
+
+@Controller('wikis')
+export class WikisController {
+  constructor(private readonly wikisService: WikisService) {}
+
+  @Role(UserType.ORGANIZER)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './files',
+        filename: editFileName,
+      }),
+    }),
+  )
+  @Post()
+  create(
+    @Request() req,
+    @Body() createWikiDto: CreateWikiDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'png|jpeg|jpg',
+        })
+        .build({
+          fileIsRequired: false,
+        }),
+    )
+    file?: Express.Multer.File,
+  ) {
+    return this.wikisService.create(createWikiDto, file, req.user);
+  }
+
+  @Public()
+  @Get()
+  findAll(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('tag') tag,
+    @Query('searchTitle') searchTitle,
+  ) {
+    return this.wikisService.findAll({ page, limit, tag, searchTitle });
+  }
+
+  @Public()
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.wikisService.findOne(+id);
+  }
+
+  @Role(UserType.ORGANIZER)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './files',
+        filename: editFileName,
+      }),
+    }),
+  )
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateWikiDto: UpdateWikiDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'png|jpeg|jpg',
+        })
+        .build({
+          fileIsRequired: false,
+        }),
+    )
+    file?: Express.Multer.File,
+  ) {
+    return this.wikisService.update(+id, updateWikiDto, file);
+  }
+
+  @Role(UserType.ORGANIZER)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.wikisService.remove(+id);
+  }
+}
