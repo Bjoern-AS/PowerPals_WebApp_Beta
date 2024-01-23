559a07c7013cf15b498955c34a67ef973a60d867
Bjoern-AS <Bjoern_schick@outlook.de>
2024-01-23 20:05:38 +0100

    Create app.module.ts

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { DatabaseModule } from './database/database.module';
+import { UsersProfilesModule } from './users-profiles/users-profiles.module';
+import { RolesGuard } from './users/roles.guard';
+import { UsersModule } from './users/users.module';
+import { WikisModule } from './wikis/wikis.module';
+import { NewsStickersModule } from './news-stickers/news-stickers.module';
+import { EventsModule } from './events/events.module';
+import { CommunitiesModule } from './communities/communities.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'client', 'build'),
+    }),
+    AuthModule,
+    UsersModule,
+    DatabaseModule,
+    UsersProfilesModule,
+    WikisModule,
+    NewsStickersModule,
+    EventsModule,
+    CommunitiesModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
+})
+export class AppModule {}
