df24e791b43642de87f68c5b4b251db2e3968fe5
Bjoern-AS <Bjoern_schick@outlook.de>
2024-01-23 20:11:33 +0100

    Create App.tsx

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,113 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Event from "./pages/Event"
+import EventDetails from "./pages/EventDetails"
+import Home from "./pages/Home"
+import Landing from "./pages/Landing"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+import Register from "./pages/Register"
+import Wiki from "./pages/Wiki"
+import WikiDetails from "./pages/WikiDetails"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import ProtectedRoute from "./pages/ProtectedRoute"
+import CreateWiki from "./pages/CreateWiki"
+import CreateEvent from "./pages/CreateEvent"
+import CreateCommunity from "./pages/CreateCommunity"
+import Users from "./pages/Users"
+import Community from "./pages/Community"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/create-wiki",
+        element: <CreateWiki />,
+      },
+      {
+        path: "/create-news-sticker",
+        element: <CreateWiki />,
+      },
+      {
+        path: "/create-event",
+        element: <CreateEvent />,
+      },
+      {
+        path: "/create-community",
+        element: <CreateCommunity />,
+      },
+      {
+        path: "/community",
+        element: <Community />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+    ],
+  },
+  {
+    path: "/wiki",
+    element: <Wiki />,
+  },
+
+  {
+    path: "/wiki/news/:id",
+    element: <WikiDetails />,
+  },
+  {
+    path: "/wiki/article/:id",
+    element: <WikiDetails />,
+  },
+  {
+    path: "/event",
+    element: <Event />,
+  },
+  {
+    path: "/event/:id",
+    element: <EventDetails />,
+  },
+])
+
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  )
+}
+
+export default App
