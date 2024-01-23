9a3f9a93181e8d3ea421aa3b7327ff78e3df6358
Bjoern-AS <Bjoern_schick@outlook.de>
2024-01-23 20:10:24 +0100

    Create wikiAPI.ts

---
+++ apps/client/src/features/wiki/wikiAPI.ts
@@ -0,0 +1,44 @@
+import { baseAxios } from "../../utils/axios"
+import { WikiDetails } from "./wikiSlice"
+
+export interface GetWikis {
+  page: number
+  limit: number
+  tag?: string
+  searchTitle?: string
+}
+export const getWiki = (id: number) => baseAxios.get(`/wikis/${id}`)
+
+export const getWikis = ({ page, limit, tag, searchTitle }: GetWikis) => {
+  const queryParams = new URLSearchParams({
+    page: String(page),
+    limit: String(limit),
+    ...(tag && { tag }),
+    ...(searchTitle && { searchTitle }),
+  })
+  return baseAxios.get(`/wikis?${queryParams.toString()}`)
+}
+
+export const deleteWiki = (id: number) => baseAxios.delete(`/wikis/${id}`)
+
+export const createWiki = (body: FormData | WikiDetails, isForm: boolean) =>
+  isForm
+    ? baseAxios.post(`/wikis`, body, {
+        headers: {
+          "Content-Type": "multipart/form-data", // Set content type to multipart/form-data
+        },
+      })
+    : baseAxios.post(`/wikis`, body)
+
+export const patchWiki = (
+  id: number,
+  body: FormData | WikiDetails,
+  isForm: boolean,
+) =>
+  isForm
+    ? baseAxios.patch(`/wikis/${id}`, body, {
+        headers: {
+          "Content-Type": "multipart/form-data", // Set content type to multipart/form-data
+        },
+      })
+    : baseAxios.patch(`/wikis/${id}`, body)
